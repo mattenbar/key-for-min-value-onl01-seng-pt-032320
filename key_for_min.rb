5948b8037761696e788f69446a66df3ffa10ef25
matthew enbar <matthew.enbar@gmail.com>
2020-03-29 14:37:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   smallest_value = nil
-  smallest_key = nil
+  smallest_key = 0
   name_hash.each do |key, value|
     if value < smallest_value  || smallest_value == nil
       smallest_key = key
