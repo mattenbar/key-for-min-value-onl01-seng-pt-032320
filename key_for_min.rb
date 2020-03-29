31ce3ae9228c31e72181074d122121065cf7789a
matthew enbar <matthew.enbar@gmail.com>
2020-03-29 14:39:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  smallest_value = 0
-  smallest_key = nil
-  name_hash.each do |key, value|
-    if value < smallest_value  || smallest_value == nil
-      smallest_key = key
-      smallest_value = value
+  lowest_key = nil
+  lowest_value = nil
+  name_hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
     end
   end
-  smallest_key
-end
+  lowest_key
+end 
