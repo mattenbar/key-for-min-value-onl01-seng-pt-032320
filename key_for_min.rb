5753307b34fd64f1dd9be811b20d178a71c35b7b
matthew enbar <matthew.enbar@gmail.com>
2020-03-29 14:22:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  smallest_key = 0
+  smallest_value = 0
   smallest_key = ""
   name_hash.map do
-    |x, value| if smallest_key > value
+    |x, value| if smallest_value > value
       smallest_key = value
   end
   end
